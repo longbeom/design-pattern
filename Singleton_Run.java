eac6a1ac33e349c6c8b03c2fd7e1121daed23255
longbeom <wowow1859@naver.com>
2022-09-14 12:52:48 +0900

    add: Singleton

---
+++ Singleton_Run.java
@@ -0,0 +1,21 @@
+class Singleton {
+    private static class singletonInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static synchronized Singleton getInstance() {
+        return singletonInstanceHolder.INSTANCE;
+    }
+}
+
+
+public class Singleton_Run {
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+        
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+        System.out.println(a == b);
+    }
+}
