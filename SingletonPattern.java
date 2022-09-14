4760d0c0cc610950d86d3b44ee65b438d2e36a10
longbeom <wowow1859@naver.com>
2022-09-14 14:05:41 +0900

    chore: Singleton Pattern

--- SingletonPattern.java
+++ SingletonPattern.java
@@ -10,7 +10,7 @@ class Singleton {
 
 
 
-public class Singleton_Run {
+public class SingletonPattern {
     public static void main(String[] args) {
         Singleton a = Singleton.getInstance();
         Singleton b = Singleton.getInstance();
